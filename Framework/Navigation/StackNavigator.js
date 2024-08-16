ba2d7374d7aa10bca08cff3452c5c27cade00356
Miracle Obafemi <miracleobafemi09@gmail.com>
2024-08-16 17:55:49 +0100

    new changes

--- Framework/Navigation/StackNavigator.js
+++ Framework/Navigation/StackNavigator.js
@@ -2,15 +2,17 @@ import { createStackNavigator } from '@react-navigation/stack';
 import { HomeScreen } from '../Screens/HomeScreen';
 import { Intro } from '../Screens/Intro';
 import { Login } from '../Screens/Login';
+import { Profile } from '../Screens/Profile';
 
 const Stack = createStackNavigator();
 
 export function StackNavigator() {
     return (
-        <Stack.Navigator>
+        <Stack.Navigator initialRouteName="Intro">
             <Stack.Screen name="Intro" component={Intro} options={{ headerShown: false }} />
-            <Stack.Screen name="Home" component={HomeScreen} options={{ headerShown: false }} />
+            <Stack.Screen name="HomeScreen" component={HomeScreen} options={{ headerShown: false }} />
             <Stack.Screen name="Login" component={Login} />
+            <Stack.Screen name="Profile" component={Profile} />
         </Stack.Navigator>
     );
 }
--- Framework/Screens/HomeScreen.js
+++ Framework/Screens/HomeScreen.js
@@ -1,12 +1,47 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Profile } from './Profile';
+import { Theme } from '../Components/Theme';
+import { Ionicons } from '@expo/vector-icons';
 
-export function HomeScreen() {
+function Home() {
     return (
-        <View>
+        <View >
             <Text>HomeScreen</Text>
+            <Text>Buy my product</Text>
         </View>
     )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
+
+const Tab = createBottomTabNavigator();
+export function HomeScreen() {
+    return (
+        <Tab.Navigator
+            screenOptions={({ route }) => ({
+                tabBarIcon: ({ focused, color }) => {
+                    let iconName;
+                    let size;
+                    if (route.name === 'Home') {
+                        size = focused ? 35 : 23
+                        iconName = focused ? 'home' : 'home-outline';
+                    }
+                    else if (route.name === 'Profile') {
+                        size = focused ? 35 : 23
+                        iconName = focused ? 'person' : 'person-outline';
+                    }
+
+                    return <Ionicons name={iconName} size={size} color={color} />;
+                },
+                tabBarActiveTintColor: Theme.colors.primary,
+                tabBarInactiveTintColor: Theme.colors.gray,
+                headerShown: false,
+            })}
+        >
+            <Tab.Screen name='Home' component={Home} />
+            <Tab.Screen name='Profile' component={Profile} />
+        </Tab.Navigator>
+    )
+}
